24ebfdd774b83d368d83268e0cbb29519c4438dd
lukDev <lukas.mauter@tum.de>
2019-07-10 21:35:58 +0200

    structure with files

---
+++ utils/data.py
@@ -0,0 +1,48 @@
+class Data:
+    # --- controller parameters ---
+    players = []
+    obstacles = []
+    gen = 0
+    overall_time = 0.
+    closest_obstacle = None
+    second_closest_obstacle = None
+
+    # --- game parameters ---
+    field_size_x = 800.
+    field_size_y = 300.
+    player_x = 100.
+    baseline = 60.
+    player_size_x = 20.
+    player_size_y = 50.
+    activation_jump_threshold = 0.75
+    obstacle_speed = 400.
+    obstacle_start_x = field_size_x + 10.
+    obstacle_size_x = 20.
+    obstacle_sizes_y = [(40., .6), (60., .4)]
+    obstacle_time_dif_min = 0.55
+    obstacle_time_dif_max = 1.5
+    jump_speed_up = 300.
+    gravity_acc = 1500.
+    no_gravity_time_long = 0.18
+    no_gravity_time_short = 0.085
+
+    # --- NEAT parameters ---
+    population_size = 170
+    disjoint_weight = 1.
+    excess_weight = 1.
+    weight_dif_weight = 0.4
+    speciation_threshold = 0.7
+    plateau_limit = 15
+    prob_crossover = 0.75
+    prob_weight_mut = 0.8
+    prob_weight_nudged = 0.9
+    weight_nudge_limit = 0.1
+    prob_new_neuron = 0.03
+    prob_new_gene = 0.05
+    current_innovation_number = 0
+
+    # --- methods ---
+    @staticmethod
+    def standard_obstacle_size_y():
+        (size_y, _) = Data.obstacle_sizes_y[0]
+        return size_y
---
+++ utils/math_helper.py
@@ -0,0 +1,48 @@
+import math
+import random
+from utils.data import Data
+
+
+def sigmoid_activation(x):
+    return 1. / (1. + math.exp(-4.9*x))
+
+
+def sigmoid_weight(x):
+    return 2. / (1. + math.exp(-4.9 * x)) - 1.
+
+
+def distance(p1x, p1y, p2x, p2y):
+    return math.sqrt((p1x - p2x)**2 + (p1y - p2y)**2)
+
+
+def center(x, y, size_x, size_y):
+    return (x + size_x / 2, y + size_y / 2)
+
+
+def random_decision(probability):
+    return random.uniform(0., 1.) <= probability
+
+
+def random_range_decision(possibilities):
+    current_range_start = 0.
+    selection = random.uniform(0., 1.)
+    for (pos, prob) in possibilities:
+        if current_range_start <= selection < current_range_start + prob:
+            return pos
+        else:
+            current_range_start += prob
+
+
+def are_rects_intersecting(x1, y1, w1, h1, x2, y2, w2, h2):
+    return ((x2 <= x1 <= x2 + w2) or (x1 <= x2 <= x1 + w1)) and ((y2 <= y1 <= y2 + h2) or (y1 <= y2 <= y1 + h1))
+
+
+def x_dif_to_activation(x1, x2):
+    return sigmoid_activation(float(abs(x1 - x2)) / float(Data.field_size_x))
+
+
+def obstacle_size_y_to_activation(size_y):
+    heights = [h for (h, _) in Data.obstacle_sizes_y]
+    max_height = max(heights)
+    min_height = min(heights)
+    return (size_y - min_height) / (max_height - min_height)
